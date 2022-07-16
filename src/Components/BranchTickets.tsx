4c787e79f1bf9e3496ce60418b702b40d5e569a2
brandonhsz <brandonhszzz@gmail.com>
2022-07-15 19:33:36 -0500

    First commit need to add FMC system

---
+++ src/Components/BranchTickets.tsx
@@ -0,0 +1,80 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import Tickets from './Tickets';
+
+import { INITIAL_STATE } from '../constants/INITIAL_STATE';
+
+const BranchTickets = () => {
+
+  const [tickets, setTickets] = useState({
+    escorza: [INITIAL_STATE],
+    revo: [INITIAL_STATE],
+    tlajo: [INITIAL_STATE]
+  })
+
+
+  const fetching = async () => {
+    const response = await fetch("https://native-request-production.up.railway.app/tickets")
+    const data = await response.json()
+    const { escorza, revo, tlajo } = data[0]
+    setTickets({
+      escorza: escorza,
+      revo: revo,
+      tlajo: tlajo
+    })
+  }
+
+
+  useEffect(() => {
+
+    fetching()
+
+  }, []);
+
+
+
+  return (
+    <View
+      style={styles.mainContainer}>
+
+      <View style={styles.ticketContainer}>
+
+        <Tickets
+          branch={"Escorza"}
+          tickets={tickets.escorza}
+        />
+
+
+        <Tickets
+          branch={"Revolucion"}
+          tickets={tickets.revo}
+        />
+
+        <Tickets
+          branch={"Tlajomulco"}
+          tickets={tickets.tlajo}
+        />
+
+
+
+      </View>
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+  },
+
+  ticketContainer: {
+    flexDirection: 'row',
+    position: 'relative',
+    top: 390,
+  }
+});
+
+
+export default BranchTickets
